a995d966377b73df9382dd59602fbece23819745
marthastamp <marta.stempel@gmail.com>
2020-07-09 22:12:39 +0200

    delete employee from database

--- EmployeeManagement.Web/Services/IEmployeeService.cs
+++ EmployeeManagement.Web/Services/IEmployeeService.cs
@@ -11,6 +11,7 @@ namespace EmployeeManagement.Web.Services
         Task<IEnumerable<Employee>> GetEmployees();
         Task<Employee> GetEmployee(int id);
         Task<Employee> UpdateEmployee(Employee updatedEmployee);
-        Task<Employee> CreateEmployee(Employee newEmployee);    
+        Task<Employee> CreateEmployee(Employee newEmployee);
+        Task DeleteEmployee(int id);
     }
 }
