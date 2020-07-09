623422850c65d7687582305ba2bfaff887bc3660
marthastamp <marta.stempel@gmail.com>
2020-07-09 22:26:21 +0200

    removed employee from employee list [Child to parent component communication]

--- EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
+++ EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
@@ -18,7 +19,23 @@ namespace EmployeeManagement.Web.Pages
         protected bool IsSelected { get; set; }
 
         [Parameter]
+        public EventCallback<int> OnEmployeeDeleted { get; set; }
+
+        [Parameter]
         public EventCallback<bool> OnEmployeeSelection { get; set; }
+
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected async Task Delete_Click()
+        {
+            await EmployeeService.DeleteEmployee(Employee.EmployeeId);
+            await OnEmployeeDeleted.InvokeAsync(Employee.EmployeeId);
+            //NavigationManager.NavigateTo("/", true);
+        }
 
         protected async Task CheckBoxChanged(ChangeEventArgs e)
         {
